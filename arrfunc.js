bcd8aae8fdc5225bb060c9d503b8cee2ef3cc7d6
namugarurasyda <2100813912@std.kyu.ac.ug>
2025-03-26 16:32:41 +0300

    first commit

---
+++ arrfunc.js
@@ -0,0 +1,18 @@
+// function CalculateBMI(weight, height){
+//     let BMI = weight/(Math.pow(height,2))
+//     return BMI
+
+// }
+// console.log(CalculateBMI(20, 2))
+
+
+
+
+
+
+
+let calculateBMI = (weight, height) =>weight/(Math.pow(height,2))
+console.log(calculateBMI(20, 2))
+
+
+
